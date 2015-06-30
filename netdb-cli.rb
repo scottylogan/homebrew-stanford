a6b9b31b7d676a636c2fc09710405d644bd96898
Scotty Logan <swl@stanford.edu>
2015-06-29 19:12:42 -0700

    changed internal names

--- netdb-cli.rb
+++ netdb-cli.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class NetDB_Cli < Formula
+class NetdbCli < Formula
   homepage 'http://www.stanford.edu/group/networking/netdb/rmi'
   url 'http://web.stanford.edu/group/networking/netdb/downloads/cli/netdb-cli-2.6.2.tar.gz'
   sha1 '52a4ed95787109c4dfb8fd2cf08d950dfb5dbec9'

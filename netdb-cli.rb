f10b0ede864c4ae6bdf3e4461b6eac01dfc47eea
Scotty Logan <swl@stanford.edu>
2015-06-29 19:14:12 -0700

    added missing dependency on jruby

--- netdb-cli.rb
+++ netdb-cli.rb
@@ -4,6 +4,7 @@ class NetdbCli < Formula
   homepage 'http://www.stanford.edu/group/networking/netdb/rmi'
   url 'http://web.stanford.edu/group/networking/netdb/downloads/cli/netdb-cli-2.6.2.tar.gz'
   sha1 '52a4ed95787109c4dfb8fd2cf08d950dfb5dbec9'
+  depends_on "jruby"
   depends_on "libnetdb-java"
 
   def install
